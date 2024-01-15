442aa1f13cd2672d1b63431037ce00051310b0c1
EricRubioSanchez <ericrubiosanchez@gmail.com>
2024-01-15 16:49:51 +0100

    navbar

--- Vista/navbar.php
+++ Vista/navbar.php
@@ -4,15 +4,25 @@ if( $_SESSION['role']=="Profe"):?>
 
 <div class="navbar">
   
-    <h2 class="align-center">Administrador</h2>
-    <a href="">Tancar Sessió</a>
-    <a href="">Horari Profesors</a>
-    <a href="">Horari Alumnat</a>
-    
+  <h2 class="align-center">Professor</h2>
+  <a href="">Tancar Sessió</a>
+  <a href="">Horari Profesors</a>
+  <a href="">Horari Alumnat</a>
   
+
 </div>
+
+
 <?php else: ?>
 
+    <div class="navbar">
+  
+  <h2 class="align-center">Administrador</h2>
+  <a href="">Tancar Sessió</a>
+  <a href="">Horari Profesors</a>
+  <a href="">Horari Alumnat</a>
+  
 
+</div>
 
 <?php endif; ?>
