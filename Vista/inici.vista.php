d3fd89f28724200e1d07b136e28feea6435f606f
pnavlani <p.navlani@sapalomera.cat>
2024-01-15 16:25:50 +0100

    Vista inici

---
+++ Vista/inici.vista.php
@@ -0,0 +1,37 @@
+
+<!-- Paras Navlani -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="../Estils/estils.css">
+	<title>Incici</title>
+</head>
+<body>
+ 
+<div class="navbar">
+  
+    <h2 class="align-center">Administrador</h2>
+    <a href="">Tancar Sessió</a>
+    <a href="">Horari Profesors</a>
+    <a href="">Horari Alumnat</a>
+    
+  
+</div>
+  <!--Opcions -->
+<div class="contenidor">
+		<h1>Inici</h1>
+
+    <button><a href="">Crear/Moure Grups</a></button> 
+    <button><a href="">Crear/Modificar Proves</a></button> 
+    <button><a href="">LListat Alumnat</a></button>
+
+  
+</div>
+</body>
+</html>
+
+
+
+
