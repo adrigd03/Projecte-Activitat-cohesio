7f3c2dde52c9485181ac1f7bcd65485524cadd5d
EricRubioSanchez <ericrubiosanchez@gmail.com>
2024-01-23 20:29:55 +0100

    Inserits mes valors a alumnes.

--- activitatcohesio.sql
+++ activitatcohesio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-01-2024 a las 14:01:55
+-- Tiempo de generación: 23-01-2024 a las 20:28:44
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -42,7 +42,18 @@ CREATE TABLE `alumnes` (
 
 INSERT INTO `alumnes` (`id`, `id_grup`, `nom`, `clase`, `grup`, `asistencia`) VALUES
 (1, 2, 'Eric', 'DAW 2', 'DAW 1', 1),
-(2, 0, 'Adrián', 'DAW', '', 0);
+(2, 0, 'Adrián', 'DAW 1', '', 0),
+(3, NULL, 'Pere', 'DAW 2', '', 0),
+(4, NULL, 'Marta', 'DAW 2', '', 0),
+(5, NULL, 'Alex', 'DAW 2', '', 0),
+(6, NULL, 'Raul', 'DAW 2', '', 1),
+(7, NULL, 'Dani', 'DAW 2', '', 0),
+(8, NULL, 'Biel', 'DAW 2', '', 0),
+(9, NULL, 'Benito', 'DAW 1', '', 0),
+(10, NULL, 'Maria', 'DAW 1', '', 0),
+(11, NULL, 'Aina', 'DAW 1', '', 0),
+(12, NULL, 'Sergi', 'DAW 1', '', 0),
+(13, NULL, 'Dani', 'DAW 1', '', 0);
 
 -- --------------------------------------------------------
 
@@ -107,7 +118,7 @@ ALTER TABLE `proves`
 -- AUTO_INCREMENT de la tabla `alumnes`
 --
 ALTER TABLE `alumnes`
-  MODIFY `id` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `grups`
