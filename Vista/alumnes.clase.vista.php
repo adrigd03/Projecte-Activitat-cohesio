da1c85930c5afa0ddd5148173763dfc62777cbf3
EricRubioSanchez <ericrubiosanchez@gmail.com>
2024-01-23 22:12:37 +0100

    ultim detall

--- Vista/alumnes.clase.vista.php
+++ Vista/alumnes.clase.vista.php
@@ -12,7 +12,7 @@
 //Crea una tabla en la que sale el nombre y si va a assistir a la actividad en la cual cada fila tiene como id el id del alumno.
 $html="<table><tr><th>Nom</th><th>Assistencia</th></tr>";
 for ($i=0; $i < count($alumnes); $i++) {
-    print_r($alumnes[$i]);
+
     if($alumnes[$i]["asistencia"]==1){$html.= "<tr id='".$alumnes[$i]["id"]."'><td>".$alumnes[$i]["nom"]."</td><td>".'<input type="checkbox" checked="checked">'."</td></tr>";}
     else{$html.= "<tr id='".$alumnes[$i]["id"]."'><td>".$alumnes[$i]["nom"]."</td><td>".'<input type="checkbox">'."</td></tr>";}
 }
