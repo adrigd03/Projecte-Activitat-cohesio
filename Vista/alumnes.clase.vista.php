8aeeebf45e2479423c7abb38140d75389f0014ab
EricRubioSanchez <ericrubiosanchez@gmail.com>
2024-01-24 20:24:33 +0100

    Afegit estils a la taula de alumnes

--- Vista/alumnes.clase.vista.php
+++ Vista/alumnes.clase.vista.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Estils/estils.css">
-    <script defer src="../Vista/alumnes.clase.js"></script>   
+    <link rel="stylesheet" href="../Estils/table.css">
+    <script defer src="../Vista/alumnes.clase.js"></script>  
     <title>Document</title>
 </head>
 <body>
 <?php require_once("../Vista/navbar.php");
+echo("<h4>".$clase."</h4>");
 //Crea una tabla en la que sale el nombre y si va a assistir a la actividad en la cual cada fila tiene como id el id del alumno.
-$html="<table><tr><th>Nom</th><th>Assistencia</th></tr>";
+$html="<table id='tabla'><tr><th>Nom</th><th>Assistencia</th></tr>";
 for ($i=0; $i < count($alumnes); $i++) {
 
     if($alumnes[$i]["asistencia"]==1){$html.= "<tr id='".$alumnes[$i]["id"]."'><td>".$alumnes[$i]["nom"]."</td><td>".'<input type="checkbox" checked="checked">'."</td></tr>";}
