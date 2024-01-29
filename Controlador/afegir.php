09f073c9eac115deb6bac42a4c2e07bd8c86125d
adriangd03 <80117534+adrigd03@users.noreply.github.com>
2024-01-29 16:57:07 +0100

    Canvis afegir
    Funcionalitats afegides

--- Controlador/afegir.php
+++ Controlador/afegir.php
@@ -1,29 +1,61 @@
-<?php 
-// Comprovem que el usuari té permís per entrar en aquesta pàgina
-// session_start();
-// if(!isset($_SESSION["correu"])){
-//     header("Location: ../Controlador/index.php");
-//     exit();
-// }
-// echo($_SESSION["correu"]);
-// echo($_SESSION["role"]);
+<?php
 
-// Comprovem que el grup existeix
-if(!isset($_GET["id"])){
+// Comprovem que el usuari té permís per entrar en aquesta pàgina
+session_start();
+if(!isset($_SESSION["correu"])){
     header("Location: ../Controlador/index.php");
     exit();
 }
-$id = $_GET["id"];
+echo($_SESSION["correu"]);
+echo($_SESSION["role"]);
 require_once("../Controlador/db.php");
 $pdo = connectar();
+
+// Comprovem que el grup existeix
+if (!isset($_GET["id"])) {
+    header("Location: ../Controlador/index.php");
+    exit();
+}
+
+$errors = '';
+$success = '';
+$id = $_GET["id"];
 $sql = "SELECT * FROM grups WHERE id = :id";
 $consulta = $pdo->prepare($sql);
 $consulta->execute(["id" => $id]);
 $grup = $consulta->fetch(PDO::FETCH_ASSOC);
-if(!$grup){
+if (!$grup) {
     header("Location: ../Controlador/index.php");
     exit();
 }
+
+// Comprovem si l'usuari ha enviat el formulari
+if (isset($_POST["afegir"])) {
+    // Obtenim els alumnes seleccionats
+    $alumnes = $_POST["alumneID"];
+    // Obtenim el grup
+    $id = $_GET["id"];
+    // Actualitzem els alumnes
+    $sql = "UPDATE alumnes SET id_grup = :id , grup = :grup WHERE id IN (" . implode(",", $alumnes) . ")";
+    $consulta = $pdo->prepare($sql);
+    $consulta->execute(["id" => $id, "grup" => $grup["nom"]]);
+
+    $success = "Alumnes afegits correctament";
+}
+
+if(isset($_POST["eliminar"])){
+    // Obtenim els alumnes seleccionats
+    $alumnes = $_POST["alumneID"];
+    // Obtenim el grup
+    $id = $_GET["id"];
+    // Actualitzem els alumnes
+    $sql = "UPDATE alumnes SET id_grup = NULL , grup = NULL WHERE id IN (" . implode(",", $alumnes) . ")";
+    $consulta = $pdo->prepare($sql);
+    $consulta->execute();
+
+    $success = "Alumnes eliminats correctament";
+}
+
 
 // Agafar tots els alumnes del grup de la taula alumnes
 $sql = "SELECT * FROM alumnes WHERE id_grup = :id";
@@ -38,16 +70,18 @@ $consulta->execute();
 $disponibles = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
 // Formatar les dades per mostrar-les a la vista html
-$alumnes = array_map(function($alumne){
-    return "<li class='list-group-item'>".$alumne["nom"]." - ". $alumne['clase'] ."</li>";
+$alumnes = array_map(function ($alumne) {
+    return "<li class='list-group-item'>" . $alumne["nom"] . " - " . $alumne['clase'] .  '<span style="display:inline-block; width= "20px";"></span>
+    ' . "<input type='checkbox' name='alumneID[]' value='" . $alumne['id'] . "'> </input>" . "</li>";
 }, $alumnes);
 $alumnes = implode("", $alumnes);
 
-$disponibles = array_map(function($alumne){
-    return "<li class='list-group-item'>".$alumne["nom"] ." - ". $alumne['clase'] . "<input type='checkbox' value='". $alumne['id'] . "'> </input>" ."</li>";
+$disponibles = array_map(function ($alumne) {
+    return "<li class='list-group-item'>" . $alumne["nom"] . " - " . $alumne['clase'] . '<span style="display:inline-block; width= "20px";"></span>
+    ' . "<input type='checkbox' name='alumneID[]' value='" . $alumne['id'] . "'> </input>" . "</li>";
 }, $disponibles);
 $disponibles = implode("", $disponibles);
 
 
+
 require_once("../Vista/afegir.vista.php");
-?>
--- Vista/afegir.vista.php
+++ Vista/afegir.vista.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Grup</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
+    
     <div class="container">
-        <h1>Nom Grup</h1>
-        
+    <a href="../Controlador/grups.php"><button type="button" class="btn-close align-self-end" aria-label="Close"></button></a>
+
+        <h1><?php echo $grup["nom"] ?></h1>
+
         <!-- Pestañas -->
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
@@ -17,31 +22,44 @@
                 <button class="nav-link" id="disponibles-tab" data-bs-toggle="tab" data-bs-target="#disponibles" type="button" role="tab" aria-controls="disponibles" aria-selected="false">Disponibles</button>
             </li>
         </ul>
-        
+
         <!-- Contenido de las pestañas -->
         <div class="tab-content" id="myTabContent">
             <!-- Pestaña de Alumnos -->
             <div class="tab-pane fade show active" id="alumnos" role="tabpanel" aria-labelledby="alumnos-tab">
                 <h3>Alumnos del grup</h3>
+                <div>
+                    <!-- Aqui mostrarem missatges d'exit o error -->
+
+                    <?php echo '<span class="text-danger">' . '<strong>' . $errors . '</strong>' . '</span>' ?>
+                    <?php echo '<span class="text-success">' . '<strong>' . $success . '</strong>' . '</span>' ?>
+
+
+                </div>
                 <!-- Aquí puedes mostrar la lista de alumnos del grupo -->
-                <ul class="list-group">
-                    <?php print_r( $alumnes );?>
-                </ul>
+                <form action=""  method="post">
+                    <ul class="list-group mb-3">
+                        <?php print_r($alumnes); ?>
+                    </ul>
+                    <button name="eliminar" type="submit" class="btn btn-primary ">Eliminar</button>
+                </form>
             </div>
-            
+
             <!-- Pestaña de Disponibles -->
             <div class="tab-pane fade" id="disponibles" role="tabpanel" aria-labelledby="disponibles-tab">
                 <h3>Alumnes disponibles</h3>
                 <!-- Aquí puedes mostrar la lista de alumnos disponibles para añadir -->
-                <ul class="list-group">
-                    <?php print_r( $disponibles );?>
-                </ul>
-                <button class="btn btn-primary">Afegir</button>
+                <form action="" method="post">
+                    <ul class="list-group mb-3">
+                        <?php print_r($disponibles); ?>
+                    </ul>
+                    <button name="afegir" type="submit" class="btn btn-primary ">Afegir</button>
+                </form>
             </div>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
 
+</html>
