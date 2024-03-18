6e91f972c9c8d1c8deb1061eea8a8dcafe4f3445
adriangd03 <80117534+adrigd03@users.noreply.github.com>
2024-03-18 16:55:16 +0100

    Imatges de grups
    Opcio per afegir imatges a un grup

--- Controlador/edicioGrups.php
+++ Controlador/edicioGrups.php
@@ -4,6 +4,54 @@ if(!isset($_SESSION["correu"])){
     header("Location: ../Controlador/index.php");
     exit();
 }
+
+if(isset($_POST["editar"])){
+    $id = $_POST["id"];
+    $nom = $_POST["nom"];
+    $imatge = $_FILES["imatge"];
+    if($imatge["size"] > 0){
+        // guardem la imatge a la carpeta de imatges
+        $target_dir = "../imatges/";
+        $target_file = $target_dir . basename($imatge["name"]);
+
+        // Si ja exiesteix una imatge amb el mateix nom, canviem el nom de la imatge abans de guardar-la
+        if(file_exists($target_file)){
+            $target_file = $target_dir . uniqid() . basename($imatge["name"]);
+            move_uploaded_file($imatge["tmp_name"], $target_file);
+        }else{
+            move_uploaded_file($imatge["tmp_name"], $target_file);
+        }
+
+        // esborrem la imatge anterior
+        require_once("../Controlador/db.php");
+        $pdo = connectar();
+        $sql = "SELECT imatge FROM grups WHERE id = :id";
+        $consulta = $pdo->prepare($sql);
+        $consulta->execute(["id"=>$id]);
+        $grup = $consulta->fetch(PDO::FETCH_ASSOC);
+        if($grup["imatge"] != null){
+            unlink($grup["imatge"]);
+        }
+
+        // Actualitzem la imatge a la base de dades
+        $imatge = $target_file;
+        $sql = "UPDATE grups SET nom = :nom, imatge = :imatge WHERE id = :id";
+        $consulta = $pdo->prepare($sql);
+        $consulta->execute(["nom"=>$nom, "imatge"=>$imatge, "id"=>$id]);
+
+
+    }else{
+        require_once("../Controlador/db.php");
+        $pdo = connectar();
+        $sql = "UPDATE grups SET nom = :nom WHERE id = :id";
+        $consulta = $pdo->prepare($sql);
+        $consulta->execute(["nom"=>$nom, "id"=>$id]);
+    }
+
+}
+
+
+
 //Obtiene todas los grupos.
 require_once("../Controlador/db.php");
 $pdo = connectar();
--- Vista/edicioGrups.vista.php
+++ Vista/edicioGrups.vista.php
@@ -9,6 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="../Estils/estils.css">
   <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <title>Inici</title>
 </head>
@@ -24,10 +25,22 @@
       <div class="card grup" id="<?php echo ($grup['id']) ?>">
         <div class="card-body">
           <h5 class="card-title text-center"><?php echo ($grup['nom']); ?></h5>
-          <button class="btn btn-dark">Canviar Nom</button>
+          <?php if($grup['imatge'] != null) :?> 
+            <!-- size of image with a relation of 16:9 -->
+            <img src="<?php echo ($grup['imatge']) ?>" class="card-img-top" alt="imatge" height="128" width="72">
+            <?php endif; ?>
+          <button class="btn btn-dark mt-3" data-bs-toggle="collapse" data-bs-target="#collapseEdit<?php echo ($grup['id']) ?>">  Editar grup</button>
+        </div>
+      </div>
+      <div class="collapse" id="collapseEdit<?php echo ($grup['id']) ?>">
+        <div class="card card-body">
+          <form action="../Controlador/edicioGrups.php" method="post" enctype="multipart/form-data">
+            <input type="text" name="id" value="<?php echo ($grup['id']) ?>" hidden>
+            <input type="text" name="nom" value="<?php echo ($grup['nom']) ?>">
+            <input type="file" name="imatge" id="imatge">
 
-          <label for="files" class="btn btn-dark">Pujar Imatge</label>
-          <input id="files" hidden type="file" accept="image/*">
+            <button type="submit" name="editar" class="btn btn-dark">Guardar</button>
+          </form>
         </div>
       </div>
     <?php endforeach; ?>
